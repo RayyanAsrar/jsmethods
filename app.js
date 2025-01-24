5bc1c1eaa35bd1741f6428b9d9235f77e8852933
Rayyan Asrar <145535106+RayyanAsrar@users.noreply.github.com>
2025-01-24 11:06:05 +0500

    new

--- app.js
+++ app.js
@@ -11,15 +11,21 @@
 // }
 // console.log(newStr);
 
-
 let str = "this is test world"
+result=""
 let a = "test"
 let b = "best"
-    
-for(let i=0 ; i<str.length ; i++){
-   let matchVal= str.slice(i , i +a.length)
-    if(matchVal===a){
-        var newStr = str.slice(0,i) + b + str.slice(i+a.length)
+let temp=""
+for(let i=0;i<str.length;i++){
+    if(str[i]===" "){
+        if(temp== a){
+            result+=b
+        }else{
+            result+=temp
+        }temp=""
+        result+=str[i]
+    }else{
+        temp+=str[i]
     }
 }
-console.log(newStr);
+console.log(result)
